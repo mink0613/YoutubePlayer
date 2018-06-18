90418c68c33824c235a438d414a40539dda95af9
Min Gyung Kwak <mink0613@sae-a.com>
2018-06-18 16:54:13 +0900

    Initial commit

---
+++ YoutubePlayer/Common/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace YoutubePlayer.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _handler;
+        private bool _isEnabled;
+
+        public RelayCommand(Action<object> handler, bool canExecute = true)
+        {
+            _handler = handler;
+            _isEnabled = canExecute;
+        }
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (value != _isEnabled)
+                {
+                    _isEnabled = value;
+                    if (CanExecuteChanged != null)
+                    {
+                        CanExecuteChanged(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _handler(parameter);
+        }
+    }
+}
---
+++ YoutubePlayer/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubePlayer.Common;
+
+namespace YoutubePlayer.ViewModel
+{
+    public class PlayerViewModel : BaseViewModel
+    {
+
+    }
+}
